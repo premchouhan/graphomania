9e0a192d53b2021b90481eb92002aa0c9303eb60
Prem Chouhan <chouhan7136@gmail.com>
2023-03-18 12:42:10 +0530

    title updated

--- frontend/src/components/carousel/carousel.jsx
+++ frontend/src/components/carousel/carousel.jsx
@@ -1,8 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
-
 function UncontrolledExample() {
   return (
-    
     <Carousel>
       <Carousel.Item>
         
@@ -12,7 +10,7 @@ function UncontrolledExample() {
           alt="TRAVEL"
         />
         <Carousel.Caption>
-          <h3>TRAVEL</h3>
+          <h1>TRAVEL</h1>
           <p>“Remember that happiness is a way of travel, not a destination.”</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -23,7 +21,7 @@ function UncontrolledExample() {
           alt="MUSIC"
         />
         <Carousel.Caption>
-          <h3>MUSIC</h3>
+          <h1>MUSIC</h1>
           <p>“Music is the language of the spirit. It opens the secret of life bringing peace, abolishing strife.”.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -35,7 +33,7 @@ function UncontrolledExample() {
           />
         
         <Carousel.Caption>
-          <h3>FOOD</h3>
+          <h1>FOOD</h1>
           <p>
           “Food for us comes from our relatives, whether they have wings or fins or roots. That is how we consider food. Food has a culture. It has a history. It has a story. It has relationships.”
           </p>
@@ -47,10 +45,8 @@ function UncontrolledExample() {
           src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg"
           alt="NATURE"
         />
-
-        
         <Carousel.Caption>
-          <h3>NATURE</h3>
+          <h1>NATURE</h1>
           <p>“Rest is not idleness, and to lie sometimes on the grass under trees on a summer's day, listening to the murmur of the water, or watching the clouds float across the sky, is by no means a waste of time.”</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -61,7 +57,7 @@ function UncontrolledExample() {
           alt="CINEMA"
         />
         <Carousel.Caption>
-          <h3>CINEMA</h3>
+          <h1>CINEMA</h1>
           <p>"Cinema is an art form, a result of dedication of a number of people."</p>
     
         </Carousel.Caption>
